f69aa239b52c7c538821386e57449eaad2a60e2e
Guilherme Oliveira <guideoliveira2197@gmail.com>
2024-05-21 21:37:26 -0300

    adcionando funcionalidade JS

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const botao = document.querySelector('.btn');
+const tela1 = document.querySelector('.tela-1');
+const tela2 = document.querySelector('.tela-2');
+
+botao.addEventListener('click', (event) =>{
+    event.preventDefault();
+
+    let campo = document.getElementById('input')
+    let mensagem = document.getElementById('sub')
+    let valor = campo.value
+
+    if(valor.indexOf('@') === -1 && valor.length <= 5){
+        mensagem.classList.remove('invisivel')
+        campo.classList.add('validacao')
+    }else{
+        tela1.classList.add('invisivel');
+        tela2.classList.remove('invisivel')
+    }
+    
+})
+
+const retorno = document.querySelector('.btn-2');
+
+retorno.addEventListener('click', () =>{
+    location.reload();
+})
